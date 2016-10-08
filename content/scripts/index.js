003bd5a129ba510036c6ff14ed2770094c09e2d3
unknown <carlodeguzman5>
2016-10-08 15:18:56 +0800

    Sample Preview

--- content/scripts/index.js
+++ content/scripts/index.js
@@ -1,17 +1,18 @@
 $(document).ready(function() {
-	
 	$(window).scroll(function(e) {
-
-		
 		if ($(window).scrollTop() > 175) {
 			$('#wrap').addClass("sticky");
-				console.log($(window).scrollTop());
-		
 		} else {
 			$('#wrap').removeClass("sticky");
 		}
-
 	});
-	
-	
+
+	$('.carousel').slick({
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		autoplay: true,
+		autoplaySpeed: 2500
+	});
+
+
 });
