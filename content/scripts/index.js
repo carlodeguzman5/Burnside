8479d35c4a662fe4a318668f304de77aeeb96eb6
Carlo Luis De Guzman <carlodeguzman5@gmail.com>
2016-12-24 15:51:08 +0800

    Backup

--- content/scripts/index.js
+++ content/scripts/index.js
@@ -14,5 +14,49 @@ $(document).ready(function() {
 		autoplaySpeed: 2500
 	});
 
+	$('#fb-logo').hover(
+		function(){
+			$('#fb-logo').attr("src", "content\\icons\\facebook-logo-colored.png");
+
+		},
+		function(){
+			$('#fb-logo').attr("src", "content\\icons\\facebook-logo.png");
+	});
+
+	$('#ig-logo').hover(
+		function(){
+			$('#ig-logo').attr("src", "content\\icons\\instagram-logo-colored.png")
+		},
+		function(){
+			$('#ig-logo').attr("src", "content\\icons\\instagram-logo.png");
+	});
+
+
+	var modal = document.getElementById('myModal');
+	// Get the image and insert it inside the modal - use its "alt" text as a caption
+	var img = document.getElementById('myImg');
+	var modalImg = document.getElementById("img01");
+	var captionText = document.getElementById("caption");
+
+	$('.gallery-image').click(function(){
+		modal.style.display = "block";
+		modalImg.src = this.src;
+		captionText.innerHTML = this.alt;
+	});
+
+	// img.onclick = function(){
+	//     modal.style.display = "block";
+	//     modalImg.src = this.src;
+	//     captionText.innerHTML = this.alt;
+	// }
+
+	// Get the <span> element that closes the modal
+	var span = document.getElementsByClassName("close")[0];
+
+	// When the user clicks on <span> (x), close the modal
+	span.onclick = function() {
+	  modal.style.display = "none";
+	}
+
 
 });
