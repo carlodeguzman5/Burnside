45881e21632f220ce0fc1ac9357c06582da20a8e
Carlo Luis De Guzman <Carlo Luis De Guzman>
2016-12-26 15:21:17 +0800

    Edited Layouts and used Bootstrap

--- content/scripts/index.js
+++ content/scripts/index.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
 	$(window).scroll(function(e) {
+		console.log($(window).scrollTop())	;
 		if ($(window).scrollTop() > 175) {
 			$('#wrap').addClass("sticky");
 		} else {
@@ -30,33 +31,4 @@ $(document).ready(function() {
 		function(){
 			$('#ig-logo').attr("src", "content\\icons\\instagram-logo.png");
 	});
-
-
-	var modal = document.getElementById('myModal');
-	// Get the image and insert it inside the modal - use its "alt" text as a caption
-	var img = document.getElementById('myImg');
-	var modalImg = document.getElementById("img01");
-	var captionText = document.getElementById("caption");
-
-	$('.gallery-image').click(function(){
-		modal.style.display = "block";
-		modalImg.src = this.src;
-		captionText.innerHTML = this.alt;
-	});
-
-	// img.onclick = function(){
-	//     modal.style.display = "block";
-	//     modalImg.src = this.src;
-	//     captionText.innerHTML = this.alt;
-	// }
-
-	// Get the <span> element that closes the modal
-	var span = document.getElementsByClassName("close")[0];
-
-	// When the user clicks on <span> (x), close the modal
-	span.onclick = function() {
-	  modal.style.display = "none";
-	}
-
-
 });
